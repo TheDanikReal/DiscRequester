f2a47f28935df3afd4c723a1e0442bb7357f62d3
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2022-11-22 11:31:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const api = new API({
     spaces: 1
 })
 
+api.interpreter.addFunction({
+  data: new FunctionBuilder()
+  .setName('includes')
+  .setValue('description', 'Check if this string has the provided texts.')
+  .setValue('use', '$includes[string;text;text;...]')
+  .setValue('returns', 'Boolean'),
+  code: async d => {
+    let r = d.unpack(d);
+    if (!r.inside) return Utils.Warn('Invalid inside provided in:', d.func);
+    let [string, ...texts] = r.splits;
+      if(!string) return Utils.Warn('Missing string in:', d.func);
+      if(!texts[0]) return Utils.Warn('Missing text to search in:', d.func);
+    let includes = false;
+    for (let text of texts) {
+      if (string.includes(text)) includes = true;
+    }
+    return {
+      code: d.code.resolve(`${d.func}[${r.inside}]`, includes)
+    };
+  }
+})
 
 api.routes.load('./routes').then(() => {
     console.log('Source loaded.')
