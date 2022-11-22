e4b4ef8acb095cb98fc14789564defeb26302c08
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2022-11-22 11:18:05 +0300

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,1 +1,12 @@
-module.exports
+module.exports = {
+    path: '/invite',
+    code: `
+    $send[200;json;$getdata[$default]
+]
+$if[$getdata[$default]==undefined;400;{
+"error": "true"
+"info": "$getdata[$default]"
+}]
+$request[$getQuery[link]]
+    `
+}
